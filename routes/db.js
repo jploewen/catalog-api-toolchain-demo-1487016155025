4aa1f7e282e2f53b1c85088be5cfac406dca3aac
jploewen <jploewen@us.ibm.com>
2017-02-26 14:57:31 -0500

    update water bottle pic

--- routes/db.js
+++ routes/db.js
@@ -70,9 +70,8 @@ var populateDB = function() {
         quantity: 71,
         description: '3 different colors to match your personality!',
         usaDollarPrice: 29.99,
-        //imgsrc:'http://upload.wikimedia.org/wikipedia/commons/0/07/Multi-use_water_bottle.JPG'
-	//imgsrc:'https://en.wikipedia.org/wiki/Main_Page#/media/File:Lufthansa_Airbus_A320-211_D-AIQT_01.jpg'
-    	imgsrc:'http://upload.wikimedia.org/wikipedia/commons/2/26/Invalidenpark%2C_Berlin-Mitte%2C_360x180%2C_160326%2C_ako.jpg'
+        imgsrc:'http://upload.wikimedia.org/wikipedia/commons/0/07/Multi-use_water_bottle.JPG'
+    	//imgsrc:'http://upload.wikimedia.org/wikipedia/commons/2/26/Invalidenpark%2C_Berlin-Mitte%2C_360x180%2C_160326%2C_ako.jpg'
     }];
 
     for (var p in products){
